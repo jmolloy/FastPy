1e98648bcd7d1ccfd76a9c9090b58b24d90decc7
James Molloy <james@jamesmolloy.co.uk>
2011-03-30 16:49:24 +0100

    Added the start of a test suite.

---
+++ tests/trivial/test.py
@@ -0,0 +1,20 @@
+#!/bin/env python
+
+import glob, subprocess, difflib, py_compile
+
+for fn in glob.glob("tests/trivial/_*.py"):
+    py_compile.compile(fn)
+    try:
+        out = subprocess.check_output(['build/fastpy', fn + 'c'], stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError, e:
+        out = e.output
+    with open("%s.out" % fn) as fd:
+        idealout = fd.read()
+    if out == idealout:
+        print "PASS: %s" % fn
+    else:
+        d = difflib.Differ()
+        c = d.compare(idealout.splitlines(), out.splitlines())
+        print "FAIL: %s" % fn
+        for line in c:
+            print line
